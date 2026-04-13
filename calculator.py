22553971529516fd582d7ae92346d8295b149d81
antiparu <131477888+antiparu@users.noreply.github.com>
2026-04-12 22:13:07 -0400

    Modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +8,21 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
-
+    return a + b
+def subtract(a,b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a,b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Divide by zero error")
+def logarithm(a,b):
+    try:
+        result = math.log(a, b)
+    except ValueError:
+        print("Invalid input for logarithm.")
+def exponent(a,b):
+    return a ** b
 
