c87db49606c6deab64d2c7b0b550b7e4d1e95264
Alejandro Castro Delgado <patribros@gmail.com>
2026-04-13 02:48:20 -0400

    Patner 2; I added calculator an tests

--- calculator.py
+++ calculator.py
@@ -1,28 +1,28 @@
-import math
+import math   #at first,  we import the math library so we can use math.log()
 
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+def add(a, b):
+    return a + b   #we return the sum of a and b
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    return a + b
-def subtract(a,b):
-    return a - b
-def multiply(a, b):
-    return a * b
-def divide(a,b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Divide by zero error")
-def logarithm(a,b):
-    try:
-        result = math.log(a, b)
-    except ValueError:
-        print("Invalid input for logarithm.")
-def exponent(a,b):
+def sub(a, b):
+    return a - b   #we return the result of the subtraction b from a
+
+def mul(a, b):
+    return a * b   #we return the product of a and b
+
+def div(a, b):
+    #we raise a ZeroDivision error if a == 0
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")  #this is the error if we cant divide by 0
+    return b / a   #otherwise, we return b / a
+
+def log(a, b):
+    # computing log of b with base using math.log(b,a)
+
+    #we must raise ValueError for invalid inputs
+
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid base or argument for logarithm")
+    return math.log(b, a)   #we compute log_a(b)
+
+def exp(a, b):
     return a ** b
-
--- test_calculator.py
+++ test_calculator.py
@@ -1,56 +1,57 @@
 import unittest
-from calculator import *
+import calculator
 
 class TestCalculator(unittest.TestCase):
+
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(calculator.add(3, 4), 7)
+        self.assertEqual(calculator.add(-2, 5), 3)
+        self.assertEqual(calculator.add(0, 0), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(calculator.sub(10, 3), 7)
+        self.assertEqual(calculator.sub(5, 10), -5)
+        self.assertEqual(calculator.sub(0, 0), 0)
+    ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
+    # def test_multiply(self):
     #     fill in code
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
-
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
+    # def test_divide(self):
     #     fill in code
     ##########################
 
-# Do not touch this
+    ######## Partner 2
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            calculator.div(0, 10)
+
+    def test_logarithm(self):
+        self.assertEqual(calculator.log(2, 8), 3)
+        self.assertEqual(calculator.log(10, 100), 2)
+        self.assertEqual(calculator.log(3, 27), 3)
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            calculator.log(1, 10)
+        with self.assertRaises(ValueError):
+            calculator.log(-2, 10)
+        with self.assertRaises(ValueError):
+            calculator.log(2, -5)
+    ##########################
+
+    ######## Partner 1
+    # def test_log_invalid_argument(self):
+    #     fill in code
+
+    # def test_hypotenuse(self):
+    #     fill in code
+
+    # def test_sqrt(self):
+    #     fill in code
+    ##########################
+
 if __name__ == "__main__":
     unittest.main()
