7bd08adc83337db722773222205b5214c86d4bb3
antiparu <carrington4044@gmail.com>
2026-04-13 13:53:41 -0400

    Updated integration tests

--- calculator.py
+++ calculator.py
@@ -1,35 +1,43 @@
 import math   #at first,  we import the math library so we can use math.log()
 
+#Josh Carrington as Partner 1
+#Alejandro Delgado as Partner 2
+
+
 def add(a, b):
     return a + b   #we return the sum of a and b
+
 
 # First example
 def square_root(a):
     if a < 0:
         raise ValueError("Value cannot be negative")
     return math.sqrt(a)
+
+
 def hypotenuse(a, b):
     return math.hypot(a, b)
+
 
 def subtract(a,b):
     return a - b
 
-def logarithm(a,b):
-    try:
-        result = math.log(a, b)
-    except ValueError:
-        print("Invalid input for logarithm.")
-        
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm.")
+    return math.log(b, a)
+
 
 def mul(a, b):
     return a * b   #we return the product of a and b
+
 
 def div(a, b):
     #we raise a ZeroDivision error if a == 0
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")  #this is the error if we cant divide by 0
-    return b / a   #otherwise, we return b / a
-
+    return a / b   #otherwise, we return b / a
 
 
 def exp(a, b):
--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,11 @@
 import unittest
 import calculator
 
+
+#Josh Carrington as Partner 1
+#Alejandro Delgado as Partner 2
+
+
 class TestCalculator(unittest.TestCase):
 
     ######### Partner 2
@@ -9,49 +14,51 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.add(-2, 5), 3)
         self.assertEqual(calculator.add(0, 0), 0)
 
-    def test_subtract(self):
-        self.assertEqual(calculator.sub(10, 3), 7)
-        self.assertEqual(calculator.sub(5, 10), -5)
-        self.assertEqual(calculator.sub(0, 0), 0)
-    ##########################
+    def test_divide(self):
+        self.assertEqual(calculator.div(8, 2), 4)
+        self.assertEqual(calculator.div(9, 3), 3)
+        self.assertEqual(calculator.div(5, 2), 2.5)
 
-    ######## Partner 1
-    # def test_multiply(self):
-    #     fill in code
-
-    # def test_divide(self):
-    #     fill in code
-    ##########################
-
-    ######## Partner 2
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            calculator.div(0, 10)
+            calculator.div(10, 0)
 
-    def test_logarithm(self):
-        self.assertEqual(calculator.log(2, 8), 3)
-        self.assertEqual(calculator.log(10, 100), 2)
-        self.assertEqual(calculator.log(3, 27), 3)
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(3, 4), 5)
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(2, 0)
 
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            calculator.log(1, 10)
+            calculator.logarithm(1, 10)
         with self.assertRaises(ValueError):
-            calculator.log(-2, 10)
+            calculator.logarithm(-2, 10)
         with self.assertRaises(ValueError):
-            calculator.log(2, -5)
-    ##########################
+            calculator.logarithm(2, -5)
 
-    ######## Partner 1
-    # def test_log_invalid_argument(self):
-    #     fill in code
+    def test_logarithm(self):
+        self.assertEqual(calculator.logarithm(2, 8), 3)
+        self.assertEqual(calculator.logarithm(10, 100), 2)
+        self.assertEqual(calculator.logarithm(3, 27), 3)
 
-    # def test_hypotenuse(self):
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(calculator.mul(2, 4), 8)
+        self.assertEqual(calculator.mul(10, 0.5), 5)
+        self.assertEqual(calculator.mul(4, -4), -16)
 
-    # def test_sqrt(self):
-    #     fill in code
-    ##########################
+    def test_sqrt(self):
+        self.assertEqual(calculator.square_root(16), 4)
+        self.assertEqual(calculator.square_root(64), 8)
+
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(3, 4), 5)
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(10, 3), 7)
+        self.assertEqual(calculator.subtract(5, 10), -5)
+        self.assertEqual(calculator.subtract(0, 0), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
